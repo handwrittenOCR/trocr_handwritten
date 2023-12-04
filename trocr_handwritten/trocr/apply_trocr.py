2c149111f80620c79a598119bbd64a78aad09d3a
agombert <arnault.gombert@pm.me>
2023-12-04 17:51:55 +0100

    :tada: apply trocr model script

---
+++ trocr_handwritten/trocr/apply_trocr.py
@@ -0,0 +1,69 @@
+from transformers import (
+    TrOCRProcessor,
+    VisionEncoderDecoderModel,
+    AutoTokenizer,
+)
+import logging
+import argparse
+import torch
+from PIL import Image
+from os.path import join
+from os import listdir
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Apply a handwritten OCR model.")
+    parser.add_argument("--PATH_DATA", type=str, help="Path to the data files")
+    parser.add_argument(
+        "--trocr_model",
+        type=str,
+        help="Path to the model",
+        default="agomberto/trocr-base-handwritten-fr",
+    )
+    parser.add_argument(
+        "--processor",
+        type=str,
+        help="Path to the processor",
+        default="microsoft/trocr-large-handwritten",
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        help="Path to the output file",
+    )
+
+    args = parser.parse_args()
+
+    logging.info("Loading model and processor...")
+    processor = TrOCRProcessor.from_pretrained(args.processor)
+
+    model = VisionEncoderDecoderModel.from_pretrained(args.trocr_model)
+    tokenizer = AutoTokenizer.from_pretrained(args.trocr_model)
+
+    # device gpu if available
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    model.to(device)
+
+    logging.info("Loading images...")
+    images = [x for x in listdir(args.PATH_DATA) if ".jpg" in x]
+    images = [Image.open(join(args.PATH_DATA, x)).convert("RGB") for x in images]
+
+    logging.info("Generating texts...")
+    pixel_values = (processor(images=images, return_tensors="pt").pixel_values).to(
+        device
+    )
+    generated_ids = model.generate(pixel_values)
+    generated_texts = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+
+    logging.info("Writing output to {}".format(args.output))
+    logging.info("Generated texts:")
+    with open(args.output, "w") as f:
+        for generated_text in generated_texts:
+            logging.info(generated_text)
+            f.write(generated_text + "\n")
