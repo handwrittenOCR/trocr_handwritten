5c50bf02d43a678adfb17b0e441f1f999d3a48aa
agombert <arnault.gombert@pm.me>
2024-12-24 17:52:38 +0100

    :beers: update sh with personal script for installation

--- trocr_handwritten/parse/prepare_data.sh
+++
@@ -1,9 +0,0 @@
-
-#Install datasets
-conda install -c conda-forge datasets -y
-
-# Connect to HuggingFace hub
-huggingface-cli login
-
-# Prepare data for training
-python prepare_data_for_training.py
