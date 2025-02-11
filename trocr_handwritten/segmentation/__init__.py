1e9c9b08f09b69de8e47b16bbfac289940401dc7
agombert <arnault.gombert@pm.me>
2025-02-11 13:38:42 +0100

    :snake: python package serice

---
+++ trocr_handwritten/segmentation/__init__.py
@@ -0,0 +1,4 @@
+from .kraken_env.line_segmenter.line_segmenter import LineSegmenter
+from .kraken_env.line_segmenter.image_processor import ImageProcessor
+
+__all__ = ["LineSegmenter", "ImageProcessor"]
